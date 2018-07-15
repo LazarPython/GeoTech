7c4f3da4837968cf626b5646e485060153859528
Lazar Bajic <lazar.python@gmail.com>
2018-07-15 13:28:29 +0200

    dodat Sass, gulp-autoprefixer, bootstrap ali u CDN formi ne znam u NPM-u, dodat browsersinc, za sada sve za stilizaciju radi ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-sass = require('gulp-sass');
+autoprefixer = require('gulp-autoprefixer'),
+sass = require('gulp-sass')
+browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
@@ -13,19 +13,43 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.scss')
+  return gulp.src(['./node_modules/bootstrap/scss/bootstrap.scss','./app/assets/styles/*.scss'])
     .pipe(sass())
-    .pipe(gulp.dest('./app/temp/styles'));
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false}))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/cssprocssor'));
 });
 
 gulp.task('watch', function() {
 
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
+
   watch('./app/index.html', function() {
-    gulp.start('html');
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.scss', function() {
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/cssprocssor/*.css')
+  .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+        .pipe(gulp.dest("./app/src/js"))
+        .pipe(browserSync.stream());
 });

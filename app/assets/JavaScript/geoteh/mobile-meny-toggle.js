ad867672f2e595abcd2a0753625d8377e1b03632
Lazar Bajic <lazar.python@gmail.com>
2018-07-28 14:28:36 +0200

    zavrsen heading napokon, zapoceto o Nama i Prijava

---
+++ app/assets/JavaScript/geoteh/mobile-meny-toggle.js
@@ -0,0 +1,6 @@
+$('.navbar-toggler-icon').click( function() {
+	$('.head__menu-icon-top').toggleClass('head__menu-icon-top--clicked'),
+	$('.head__menu-icon-middle').toggleClass('head__menu-icon-middle--clicked'),
+	$('.head__menu-icon-bottom').toggleClass('head__menu-icon-bottom--clicked');
+});
+
